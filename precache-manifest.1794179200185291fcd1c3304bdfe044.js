3c77e196c40187821d40cc11036f23926eed242b
Igor Pontes <igoppop@gmail.com>
2020-09-14 23:10:14 +0100

    Updates

---
+++ precache-manifest.1794179200185291fcd1c3304bdfe044.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126ff7bae786664d4535ce9bf4226be9",
+    "url": "/cardmemorygame/index.html"
+  },
+  {
+    "revision": "14ee4216cdffa2e37328",
+    "url": "/cardmemorygame/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "19e3dab5ece0a922ccc0",
+    "url": "/cardmemorygame/static/css/main.3fbaf358.chunk.css"
+  },
+  {
+    "revision": "14ee4216cdffa2e37328",
+    "url": "/cardmemorygame/static/js/2.2b049d2d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/cardmemorygame/static/js/2.2b049d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e3dab5ece0a922ccc0",
+    "url": "/cardmemorygame/static/js/main.c08c049e.chunk.js"
+  },
+  {
+    "revision": "08d7bfbb67d7f44b89a5",
+    "url": "/cardmemorygame/static/js/runtime-main.f653b466.js"
+  }
+]);
